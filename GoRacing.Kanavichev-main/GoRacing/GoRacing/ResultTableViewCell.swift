d346aec6461cbcc9daa75df4ae12d8e1f468d7f2
Kanavichev Alexey <97598151+maestroAlexey@users.noreply.github.com>
2023-04-03 12:00:18 +0300

    Add files via upload

---
+++ GoRacing.Kanavichev-main/GoRacing/GoRacing/ResultTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ResultTableViewCell.swift
+//  GoRacing
+//
+//  Created by Канавичев Алексей Александрович on 21.01.2023.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var namePlayer: UILabel!
+    @IBOutlet weak var gameResult: UILabel!
+    @IBOutlet weak var timeResult: UILabel!
+    }
